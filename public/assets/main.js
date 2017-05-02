5cbc3a1ec553ee8ef3293281e1753c10c0c744bb
Elana Stettin <Elana@dhcp-10-105-143-110.wireless.northwestern.private>
2017-05-02 14:46:44 -0500

    asthetic changes

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,29 +1,49 @@
 $(document).ready(function() {
+    menu_resize();
+    $(".menu-responsive > .menu-item.menu-has-children > .menu-link + .menu-children").hide();
 
-    // fix menu when passed
-    $('.masthead')
-        .visibility({
-            once: false,
-            onBottomPassed: function() {
-                $('.fixed.menu').transition('fade in');
-            },
-            onBottomPassedReverse: function() {
-                $('.fixed.menu').transition('fade out');
-            }
-        });
-
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-        .sidebar('attach events', '.toc.item');
-
-    $('.special.cards .image').dimmer({
-        on: 'hover'
+    $(".menu-responsive > .menu-item.menu-has-children > .menu-link").click(function() {
+        $(this).parent().toggleClass('active');
+        $('.menu-responsive > .menu-item.menu-has-children > .menu-link').focus().toggleClass("expanded");
+        $(this).focus().children(".fa").toggleClass("fa-angle-down fa-angle-up");
+        $(this).siblings('.menu-children').slideToggle('swing');
+        if ($(this).parent(":not(:focus)").hasClass('active')) {
+            $(this).parent(":not(:focus)").removeClass('active')
+            $(this).parent().siblings(".menu-item.menu-has-children").children(".menu-link:not(:focus)").removeClass("expanded");
+            $(this).parent().siblings(".menu-item.menu-has-children").children(".menu-link:not(:focus)").siblings().slideUp();
+            $(this).parent().siblings(".menu-item.menu-has-children").children(".menu-link:not(:focus)").children(".fa.fa-angle-up").removeClass('fa-angle-up').addClass('fa-angle-down');
+        }
     });
+
+    function menu_resize() {
+        if (($("body").width()/16) < 48) {
+            $(".menu-responsive").removeClass("menu-horizontal");
+            $(".menu-responsive").addClass("menu-hamburger");
+        } else {
+            $(".menu-responsive").removeClass("menu-hamburger");
+            $(".menu-responsive").addClass("menu-horizontal");
+            $(".menu-responsive.menu-horizontal").show();
+        }
+    };
+
+    $(window).on('load resize', function() {
+        menu_resize();
+    });
+
+    $(".fa.fa-mob").click(function() {
+        $(".menu-responsive.menu-hamburger").slideToggle();
+        $(".fa-mob").toggleClass("fa fa-bars").toggleClass("remove icon");
+    });
+
+    $(window).data("width", $(this).width()).resize(function() {
+        if ($(".menu-responsive.menu-hamburger").is(':visible')) {
+            $(".menu-responsive.menu-hamburger").hide();
+            $(".fa-mob").addClass("fa fa-bars").removeClass("remove icon");
+        }
+    });
+
     $('.shape').shape();
 
-    $('.ui.menu .ui.dropdown').dropdown({
-        on: 'hover'
-    });
     $('.ui.menu a.item')
         .on('click', function() {
             $(this)
@@ -38,5 +58,7 @@ $(document).ready(function() {
         'direction': 'downward',
         'fullTextSearch': true
     });
+
+    
 
 });
