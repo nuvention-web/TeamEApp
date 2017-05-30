820ba2202ad3d10691e02060af0daaa76463d235
Elana Stettin <Elana@dhcp-10-105-178-44.wireless.northwestern.private>
2017-05-30 12:45:25 -0500

    Revert "incorporate major into rank"
    This reverts commit 2de8280cb295e8f2f21f5556987466a231b95c78.

--- public/controllers/ProgramController.js
+++ public/controllers/ProgramController.js
@@ -25,7 +25,6 @@ app.controller('ProgramCtrl', ['$scope', 'Programs', 'CurrentUser', '$routeParam
         }, $scope.ranked_programs);
 
     });
-    console.log($scope.ranked_programs);
 
     $scope.index = 0;
 
